6ffce925f7c3c1421e9b1f9ca107898ed47e1fcd
Abdulrahman <abdulrahmanmahfudh@gmail.com>
2020-05-03 18:12:45 +0300

    RecyclerView Used to Replace ListView
    Navigation Drawer included

--- app/src/main/java/com/example/notekeeper/NoteListActivity.kt
+++ app/src/main/java/com/example/notekeeper/NoteListActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import kotlinx.android.synthetic.main.activity_note_list.*
 import kotlinx.android.synthetic.main.content_note_list.*
@@ -16,23 +17,18 @@ class NoteListActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { _ ->
-            val activityIntent = Intent(this,MainActivity :: class.java)
+            val activityIntent = Intent(this,NoteActivity :: class.java)
             startActivity(activityIntent)
         }
 
-        listNotes.adapter = ArrayAdapter(this,
-        android.R.layout.simple_list_item_1,
-        DataManager.notes)
+            listItems.layoutManager = LinearLayoutManager(this)
+            listItems.adapter = NoteRecyclerAdapter(this, DataManager.notes)
 
-        listNotes.setOnItemClickListener{parent, view, position, id ->
-            val activityIntent = Intent(this, MainActivity :: class.java)
-            activityIntent.putExtra(NOTE_POSITION,position)
-            startActivity(activityIntent)
-        }
     }
 
     override fun onResume() {
         super.onResume()
-        (listNotes.adapter as ArrayAdapter<NoteInfo>).notifyDataSetChanged()
+        listItems.adapter?.notifyDataSetChanged()
+
     }
 }
